98af1ae429ba0aee67404ffc1bf6dc7516d2112e
816778 <816778@unizar.com>
2024-09-16 17:57:12 +0200

    ej

--- p1/labSession1/line2DFittingSVD_v2.py
+++ p1/labSession1/line2DFittingSVD_v2.py
@@ -36,7 +36,7 @@ def drawLine(l,strFormat,lWidth):
     plt.plot([p_l_y[0], p_l_x[0]], [p_l_y[1], p_l_x[1]], strFormat, linewidth=lWidth)
 
 
-if __name__ == '__main__':
+def ej0():
     np.set_printoptions(precision=4,linewidth=1024,suppress=True)
 
     # This is the ground truth
@@ -77,7 +77,11 @@ if __name__ == '__main__':
     # we want to solve the equation x.T @ l = 0
     u, s, vh = np.linalg.svd(x.T) # svd function returns vh which is the tranpose version of V matrix.
     sM = scAlg.diagsvd(s, u.shape[0], vh.shape[0])  # svd function returns the diagonal s values instead of the S matrix. 
-    l_ls = vh[-1, :]
+    l_ls = vh[-1, :] # The last row of vh is the row of V corresponding to the smallest singular value
+  
+    print(u)
+    print(s.shape)
+    print(vh.shape)
 
     # Notice that the input matrix A of the svd has been decomposed such that A = u @ sM @ vh 
 
@@ -94,3 +98,139 @@ if __name__ == '__main__':
     plt.plot(xProjectedOnTheLine[0,:], xProjectedOnTheLine[1, :], 'bx')
     plt.show()
     print('End')
+
+def ej1():
+    np.set_printoptions(precision=4,linewidth=1024,suppress=True)
+
+    # This is the ground truth
+    l_GT = np.array([[2], [1], [-1500]])
+
+    plt.figure(1)
+    plt.plot([-100, 1800], [0, 0], '--k', linewidth=1)
+    plt.plot([0, 0], [-100, 1800], '--k', linewidth=1)
+    # Draw the line segment p_l_x to  p_l_y
+    drawLine(l_GT, 'g-', 1)
+    plt.draw()
+    plt.axis('equal')
+
+    print('Click in the image to continue...')
+    plt.waitforbuttonpress()
+
+    ## Generating points lying on the line but adding perpendicular Gaussian noise
+    l_GTNorm = l_GT/np.sqrt(np.sum(l_GT[0:2]**2, axis=0)) #Normalized the line with respect to the normal norm
+
+    x_l0 = np.vstack((-l_GTNorm[0:2]*l_GTNorm[2],1))  #The closest point of the line to the origin
+    plt.plot([0, x_l0[0,0]], [0, x_l0[1,0]], '-r')
+    plt.draw()
+
+    # mu = np.arange(-1000, 1000, 250)
+    # noiseSigma = 15 #Standard deviation
+    # xGT = x_l0 + np.array([[0, -1, 0], [1, 0, 0], [0, 0, 0]]) @ (l_GTNorm * mu)
+    # x = xGT + np.diag([1, 1, 0]) @ np.random.normal(0, noiseSigma, (3, len(mu)))
+
+    xGT = np.loadtxt('x2DGTLineFittingSVD.txt')
+    x = np.loadtxt('x2DLineFittingSVD.txt')
+    plt.plot(xGT[0, :], xGT[1, :], 'b.')
+    plt.plot(x[0, :], x[1, :], 'rx')
+    plt.draw()
+    print('Click in the image to continue...')
+    plt.waitforbuttonpress()
+
+    # Get only first and last points
+    x = x[:,[0,-1]]
+    xGT = xGT[:,[0,-1]]
+
+
+    ## Fit the least squares solution from points with noise using svd
+    # we want to solve the equation x.T @ l = 0
+    u, s, vh = np.linalg.svd(x.T) # svd function returns vh which is the tranpose version of V matrix.
+    sM = scAlg.diagsvd(s, u.shape[0], vh.shape[0])  # svd function returns the diagonal s values instead of the S matrix. 
+    l_ls = vh[-1, :] # The last row of vh is the row of V corresponding to the smallest singular value
+
+    print(u.shape)
+    print(s.shape)
+    print(vh.shape)
+
+    # Notice that the input matrix A of the svd has been decomposed such that A = u @ sM @ vh 
+
+    drawLine(l_ls, 'r--', 1)
+    plt.draw()
+    plt.waitforbuttonpress()
+
+
+    ## Project the points on the line using SVD
+    # If all the points are lying on the line s[2] = 0, therefore we impose it
+    #s[2] = 0
+    xProjectedOnTheLine = (u @ scAlg.diagsvd(s, u.shape[0], vh.shape[0]) @ vh).T
+    xProjectedOnTheLine /= xProjectedOnTheLine[2, :]
+
+    plt.plot(xProjectedOnTheLine[0,:], xProjectedOnTheLine[1, :], 'bx')
+    plt.show()
+    print('End')
+
+def ej2():
+    np.set_printoptions(precision=4,linewidth=1024,suppress=True)
+
+    # This is the ground truth
+    l_GT = np.array([[2], [1], [-1500]])
+
+    plt.figure(1)
+    plt.plot([-100, 1800], [0, 0], '--k', linewidth=1)
+    plt.plot([0, 0], [-100, 1800], '--k', linewidth=1)
+    # Draw the line segment p_l_x to  p_l_y
+    drawLine(l_GT, 'g-', 1)
+    plt.draw()
+    plt.axis('equal')
+
+    print('Click in the image to continue...')
+    plt.waitforbuttonpress()
+
+    ## Generating points lying on the line but adding perpendicular Gaussian noise
+    l_GTNorm = l_GT/np.sqrt(np.sum(l_GT[0:2]**2, axis=0)) #Normalized the line with respect to the normal norm
+
+    x_l0 = np.vstack((-l_GTNorm[0:2]*l_GTNorm[2],1))  #The closest point of the line to the origin
+    plt.plot([0, x_l0[0,0]], [0, x_l0[1,0]], '-r')
+    plt.draw()
+
+    # mu = np.arange(-1000, 1000, 250)
+    # noiseSigma = 15 #Standard deviation
+    # xGT = x_l0 + np.array([[0, -1, 0], [1, 0, 0], [0, 0, 0]]) @ (l_GTNorm * mu)
+    # x = xGT + np.diag([1, 1, 0]) @ np.random.normal(0, noiseSigma, (3, len(mu)))
+
+    xGT = np.loadtxt('x2DGTLineFittingSVD.txt')
+    x = np.loadtxt('x2DLineFittingSVD.txt')
+    plt.plot(xGT[0, :], xGT[1, :], 'b.')
+    plt.plot(x[0, :], x[1, :], 'rx')
+    plt.draw()
+    print('Click in the image to continue...')
+    plt.waitforbuttonpress()
+
+    ## Fit the least squares solution from points with noise using svd
+    # we want to solve the equation x.T @ l = 0
+    u, s, vh = np.linalg.svd(xGT.T) # svd function returns vh which is the tranpose version of V matrix.
+    sM = scAlg.diagsvd(s, u.shape[0], vh.shape[0])  # svd function returns the diagonal s values instead of the S matrix. 
+    l_ls = vh[-1, :] # The last row of vh is the row of V corresponding to the smallest singular value
+
+    print(u.shape)
+    print(s.shape)
+    print(vh.shape)
+
+    # Notice that the input matrix A of the svd has been decomposed such that A = u @ sM @ vh 
+
+    drawLine(l_ls, 'r--', 1)
+    plt.draw()
+    plt.waitforbuttonpress()
+
+
+    ## Project the points on the line using SVD
+    s[2] = 0  # If all the points are lying on the line s[2] = 0, therefore we impose it
+    xProjectedOnTheLine = (u @ scAlg.diagsvd(s, u.shape[0], vh.shape[0]) @ vh).T
+    xProjectedOnTheLine /= xProjectedOnTheLine[2, :]
+
+    plt.plot(xProjectedOnTheLine[0,:], xProjectedOnTheLine[1, :], 'bx')
+    plt.show()
+    print('End')
+    
+
+if __name__ == '__main__':
+    ej0()
