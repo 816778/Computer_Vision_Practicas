7675a87fe7dc4a84d23016993c78552cca3090d8
816778 <816778@unizar.com>
2024-10-23 17:39:40 +0200

    lab 4 squeleton

---
+++ p4/Laboratory4.md
@@ -0,0 +1,12 @@
+# Lab 4: Bundle Adjustment and Multiview Geometry
+
+## 1. Line fitting example using least-squares
+
+## 2. Pose estimation and bundle adjustment
+
+### 2.1 Bundle adjustment from two views
+
+## 3. Perspective-N-Point pose estimation of camera three
+
+## 4. Bundle adjustment from 3 views
+
---
+++ p4/utils/plot_utils.py
@@ -0,0 +1,101 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def createPlot(image_path=None):
+    # Cargar la imagen y convertirla a RGB
+    if image_path is not None:
+        img = cv2.cvtColor(cv2.imread(image_path), cv2.COLOR_BGR2RGB)
+
+    # Crear una figura para la imagen
+    plt.figure()
+    if image_path is not None:
+        plt.imshow(img)
+        plt.axis('on')
+
+def plotAndWait(title='Grafico'):
+
+    # Título y visualización
+    plt.title(title)
+    plt.draw()  # Actualizar la figura
+    print('Click en la imagen para continuar...')
+    plt.waitforbuttonpress()  # Espera un click para continuar
+    plt.close()
+
+def plotAndClose(title='Grafico'):
+
+    # Título y visualización
+    plt.title(title)
+    plt.draw() 
+    plt.show()
+    print('Close the figure to continue. Left button for orbit, right button for zoom.')
+    plt.close()
+
+
+def plotResidual(x,xProjected,strStyle):
+    """
+        Plot the residual between an image point and an estimation based on a projection model.
+         -input:
+             x: Image points.
+             xProjected: Projected points.
+             strStyle: Line style.
+         -output: None
+         """
+
+    for k in range(x.shape[1]):
+        plt.plot([x[0, k], xProjected[0, k]], [x[1, k], xProjected[1, k]], strStyle)
+
+def plotNumberedImagePoints(x,strColor,offset):
+    """
+        Plot indexes of points on a 2D image.
+         -input:
+             x: Points coordinates.
+             strColor: Color of the text.
+             offset: Offset from the point to the text.
+         -output: None
+         """
+    for k in range(x.shape[1]):
+        plt.text(x[0, k]+offset, x[1, k]+offset, str(k), color=strColor)
+
+def plotNumbered3DPoints(ax, X,strColor, offset):
+    """
+        Plot indexes of points on a 3D plot.
+         -input:
+             ax: axis handle
+             X: Points coordinates.
+             strColor: Color of the text.
+             offset: Offset from the point to the text.
+         -output: None
+         """
+    for k in range(X.shape[1]):
+        ax.text(X[0, k]+offset, X[1, k]+offset, X[2,k]+offset, str(k), color=strColor)
+
+def draw3DLine(ax, xIni, xEnd, strStyle, lColor, lWidth):
+    """
+    Draw a segment in a 3D plot
+    -input:
+        ax: axis handle
+        xIni: Initial 3D point.
+        xEnd: Final 3D point.
+        strStyle: Line style.
+        lColor: Line color.
+        lWidth: Line width.
+    """
+    ax.plot([np.squeeze(xIni[0]), np.squeeze(xEnd[0])], [np.squeeze(xIni[1]), np.squeeze(xEnd[1])], [np.squeeze(xIni[2]), np.squeeze(xEnd[2])],
+            strStyle, color=lColor, linewidth=lWidth)
+
+def drawRefSystem(ax, T_w_c, strStyle, nameStr):
+    """
+        Draw a reference system in a 3D plot: Red for X axis, Green for Y axis, and Blue for Z axis
+    -input:
+        ax: axis handle
+        T_w_c: (4x4 matrix) Reference system C seen from W.
+        strStyle: lines style.
+        nameStr: Name of the reference system.
+    """
+    draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 0:1], strStyle, 'r', 1)
+    draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 1:2], strStyle, 'g', 1)
+    draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 2:3], strStyle, 'b', 1)
+    ax.text(np.squeeze( T_w_c[0, 3]+0.1), np.squeeze( T_w_c[1, 3]+0.1), np.squeeze( T_w_c[2, 3]+0.1), nameStr)
