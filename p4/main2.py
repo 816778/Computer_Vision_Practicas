d66ee7490f6899ed0e11ce8f362e7ea239e8aca0
hsunekichi <hsunekichi@gmail.com>
2024-11-02 20:48:35 +0100

    Bundle adjustment works with ground truth matches and full rotation matrix, does not converge with sift points

--- p4/main2.py
+++ p4/main2.py
@@ -28,77 +28,114 @@ import scipy.io as sio
 import utils.utils as utils
 import utils.plot_utils as plot_utils
 
+work_dir = "/home/hsunekichi/Desktop/Computer_Vision_Practicas/p4/"
+
 
 def load_data():
-    T_wc1 = np.loadtxt('data/T_w_c1.txt')
-    T_wc2 = np.loadtxt('data/T_w_c2.txt')
-    T_wc3 = np.loadtxt('data/T_w_c3.txt')
-    K_c = np.loadtxt('data/K_c.txt')
-    X_w = np.loadtxt('data/X_w.txt')
-    x1Data = np.loadtxt('data/x1Data.txt')
-    x2Data = np.loadtxt('data/x2Data.txt')
-    x3Data = np.loadtxt('data/x3Data.txt')
+    T_wc1 = np.loadtxt(work_dir+"data/T_w_c1.txt")
+    T_wc2 = np.loadtxt(work_dir+"data/T_w_c2.txt")
+    T_wc3 = np.loadtxt(work_dir+"data/T_w_c3.txt")
+    K_c = np.loadtxt(work_dir+"data/K_c.txt")
+    X_w = np.loadtxt(work_dir+"data/X_w.txt")
+    x1Data = np.loadtxt(work_dir+"data/x1Data.txt")
+    x2Data = np.loadtxt(work_dir+"data/x2Data.txt")
+    x3Data = np.loadtxt(work_dir+"data/x3Data.txt")
     return T_wc1, T_wc2, T_wc3, K_c, X_w, x1Data, x2Data, x3Data
 
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     np.set_printoptions(precision=4,linewidth=1024,suppress=True)
 
-    T_wc1, T_wc2, T_wc3, K_c, X_w, x1Data, x2Data, x3Data = load_data()
+    _, _, _, K_c, _, x1Data, x2Data, x3Data = load_data()
 
     #plot_utils.plot_3D_scene(T_wc1, T_wc2, T_wc3, X_w)
     
-    im1_pth = 'images/image1.png'
-    im2_pth = 'images/image2.png'
-    im3_pth = 'images/image3.png'
+    im1_pth = work_dir+"images/image1.png"
+    im2_pth = work_dir+"images/image2.png"
+    im3_pth = work_dir+"images/image3.png"
 
-    image1 = cv2.imread('images/image1.png')
-    image2 = cv2.imread('images/image2.png')
-    image3 = cv2.imread('images/image3.png')
-    
-    kpCv1 = [cv2.KeyPoint(x1Data[0, k], x1Data[1, k], 1) for k in range(x1Data.shape[1])]
-    kpCv2 = [cv2.KeyPoint(x2Data[0, k], x2Data[1, k], 1) for k in range(x2Data.shape[1])]
-    kpCv3 = [cv2.KeyPoint(x3Data[0, k], x3Data[1, k], 1) for k in range(x3Data.shape[1])]
+    image1 = cv2.imread(im1_pth)
+    image2 = cv2.imread(im2_pth)
+    image3 = cv2.imread(im3_pth)
 
-    matchesList12 = np.hstack((np.arange(x1Data.shape[1]).reshape(-1, 1),
-                               np.arange(x2Data.shape[1]).reshape(-1, 1),
-                               np.ones((x1Data.shape[1], 1))))
-    
-    minDist = 100
-    distRatio = 0.85
-    dMatchesList12, keypoints1, keypoints2 = utils.visualize_matches_with_threshold(im1_pth, im2_pth, minDist, distRatio)
-    dMatchesList13, keypoints1, keypoints3 = utils.visualize_matches_with_threshold(im1_pth, im3_pth, minDist, distRatio)
-   
-    matchesList12 = utils.matchesListToIndexMatrix(dMatchesList12)
-    matchesList13 = utils.matchesListToIndexMatrix(dMatchesList13)
+    # Construct the matches
+    #kpCv1 = []
+    #kpCv2 = []
+    #kpCv3 = []
+    #for kPoint in range(x1Data.shape[1]):
+    #    kpCv1.append(cv2.KeyPoint(x1Data[0, kPoint], x1Data[1, kPoint],1))
+    #    kpCv2.append(cv2.KeyPoint(x2Data[0, kPoint], x2Data[1, kPoint],1))
+    #    kpCv3.append(cv2.KeyPoint(x3Data[0, kPoint], x3Data[1, kPoint],1))
+#
+    #matchesList12 = np.hstack((np.reshape(np.arange(0, x1Data.shape[1]),(x2Data.shape[1],1)),
+    #                                    np.reshape(np.arange(0, x1Data.shape[1]), (x1Data.shape[1], 1)),np.ones((x1Data.shape[1],1))))
+#
+    #matchesList13 = np.hstack((np.reshape(np.arange(0, x1Data.shape[1]),(x3Data.shape[1],1)),
+    #                                    np.reshape(np.arange(0, x1Data.shape[1]), (x1Data.shape[1], 1)),np.ones((x1Data.shape[1],1))))
+#
+    ##matchesList13 = matchesList12
+    #dMatchesList12 = utils.indexMatrixToMatchesList(matchesList12)
+    #dMatchesList13 = utils.indexMatrixToMatchesList(matchesList13)
 
     # Matched points in numpy from list of DMatches
-    srcPts = np.float32([kpCv1[m.queryIdx].pt for m in matchesList12]).reshape(len(matchesList12), 2)
-    dstPts = np.float32([kpCv2[m.trainIdx].pt for m in matchesList12]).reshape(len(matchesList12), 2)
+    #srcPts12 = np.float32([kpCv1[m.queryIdx].pt for m in dMatchesList12])
+    #dstPts12 = np.float32([kpCv2[m.trainIdx].pt for m in dMatchesList12])
 
+    #srcPts13 = np.float32([kpCv1[m.queryIdx].pt for m in dMatchesList13])
+    #dstPts13 = np.float32([kpCv3[m.trainIdx].pt for m in dMatchesList12])
+
+
+    x1 = np.vstack((x1Data, np.ones((1, x1Data.shape[1]))))
+    x2 = np.vstack((x2Data, np.ones((1, x1Data.shape[1]))))
+    matches12, srcPts12, dstPts12 = utils.do_matches(im1_pth, im2_pth, option=1)
+    matches13, srcPts13, dstPts13 = utils.do_matches(im1_pth, im3_pth, option=1)
+   
     # Matched points in homogeneous coordinates
-    x1 = np.vstack((srcPts.T, np.ones((1, srcPts.shape[0]))))
-    x2 = np.vstack((dstPts.T, np.ones((1, dstPts.shape[0]))))
-    print(x1.shape)
+    srcPts12 = np.vstack((srcPts12.T, np.ones((1, srcPts12.shape[0]))))
+    dstPts12 = np.vstack((dstPts12.T, np.ones((1, dstPts12.shape[0]))))
 
-    F = utils.estimate_fundamental_8point(x1, x2)
+    srcPts13 = np.vstack((srcPts13.T, np.ones((1, srcPts13.shape[0]))))
+    dstPts13 = np.vstack((dstPts13.T, np.ones((1, dstPts13.shape[0]))))
+                         
 
-    E_21 = K_c @ F @ K_c
+    #F12 = utils.estimate_fundamental_8point(srcPts12, dstPts12)
+    #F13 = utils.estimate_fundamental_8point(srcPts13, dstPts13)
+
+    num_iterations = 1000
+    threshold = 0.5
+    F12, _ = utils.ransac_fundamental_matrix(matches12, num_iterations, threshold)
+    F13, _ = utils.ransac_fundamental_matrix(matches13, num_iterations, threshold)
+
+    E_12 = K_c.T @ F12 @ K_c
+    E_13 = K_c.T @ F13 @ K_c
 
     # Descomponer la matriz esencial E en 4 posibles soluciones
-    R1, R2, t, _ = utils.decompose_essential_matrix(E_21)
+    R12_1, R12_2, t12, _ = utils.decompose_essential_matrix(E_12)
+    R13_1, R13_2, t13, _ = utils.decompose_essential_matrix(E_13)
+
     # Seleccionar la solución correcta triangulando los puntos 3D
-    R, t = utils.select_correct_pose(R1, R2, t, K_c, K_c, x1, x2)
+    R12, t12 = utils.select_correct_pose(R12_1, R12_2, t12, K_c, K_c, srcPts12, dstPts12)
+    R13, t13 = utils.select_correct_pose(R13_1, R13_2, t13, K_c, K_c, srcPts13, dstPts13)
 
 
-    T_wc1_opt, T_wc2_opt, T_wc3_opt, X_w_opt = utils.run_bundle_adjustment(T_wc1, T_wc2, T_wc3, K_c, X_w, x1Data, x2Data, x3Data)
+
+    T_wc1 = np.eye(4)
+    T_wc2 = utils.ensamble_T(R12, t12)
+    T_wc3 = utils.ensamble_T(R13, t13)
+    P1 = K_c @ T_wc1[:3, :]
+    P2 = K_c @ T_wc2[:3, :]
+
+    X_w = utils.triangulate_points(P1, P2, srcPts12, dstPts12)
+
+
+    T_wc1_opt, T_wc2_opt, T_wc3_opt, X_w_opt = utils.run_bundle_adjustmentFull(T_wc1, T_wc2, T_wc3, K_c, X_w, x1Data, x2Data, x3Data)
 
     # Step 6: Visualize Optimized Projection
     x1_p_opt = utils.project_points(K_c, T_wc1_opt, X_w_opt)
     x2_p_opt = utils.project_points(K_c, T_wc2_opt, X_w_opt)
     x3_p_opt = utils.project_points(K_c, T_wc3_opt, X_w_opt)
 
-    plot_utils.visualize_projection(image1, x1Data, x1_p_opt, 'Image 1 - Optimized Projection')
-    plot_utils.visualize_projection(image2, x2Data, x2_p_opt, 'Image 2 - Optimized Projection')
-    plot_utils.visualize_projection(image3, x3Data, x3_p_opt, 'Image 3 - Optimized Projection')
+    plot_utils.visualize_projection(image1, x1Data, x1_p_opt, "Image 1 - Optimized Projection")
+    plot_utils.visualize_projection(image2, x2Data, x2_p_opt, "Image 2 - Optimized Projection")
+    plot_utils.visualize_projection(image3, x3Data, x3_p_opt, "Image 3 - Optimized Projection")
--- p4/utils/utils.py
+++ p4/utils/utils.py
@@ -102,7 +102,6 @@ def is_valid_solution(R, t, K1, K2, pts1, pts2):
     pts_3d = triangulate_points(P1, P2, pts1, pts2)
     if pts_3d.shape[0] == 3:
         pts_3d = pts_3d.T
-    print("Forma de pts_3d:", pts_3d.shape)
 
     # Verificar que los puntos tengan la coordenada Z positiva (delante de ambas cámaras)
     pts_cam1 = pts_3d[:, 2]  # Coordenada Z en la cámara 1
@@ -141,6 +140,16 @@ def select_correct_pose(R1, R2, t, K1, K2, pts1, pts2):
     raise ValueError("No se encontró una solución válida.")
 
 
+def ensamble_T(R_w_c, t_w_c) -> np.array:
+    """
+    Ensamble the a SE(3) matrix with the rotation matrix and translation vector.
+    """
+    T_w_c = np.zeros((4, 4))
+    T_w_c[0:3, 0:3] = R_w_c
+    T_w_c[0:3, 3] = t_w_c
+    T_w_c[3, 3] = 1
+    return np.linalg.inv(T_w_c)
+
 
 def project_points(K, T, X_w):
     if T.shape == (3, 4):
@@ -158,19 +167,7 @@ def project_points(K, T, X_w):
     return x_proj
 
 
-def ensamble_T(R_w_c, t_w_c) -> np.array:
-    """
-    Ensamble the a SE(3) matrix with the rotation matrix and translation vector.
-    """
-    T_w_c = np.zeros((4, 4))
-    T_w_c[0:3, 0:3] = R_w_c
-    T_w_c[0:3, 3] = t_w_c
-    T_w_c[3, 3] = 1
-    return np.linalg.inv(T_w_c)
-
-
-
-def project_points_exp_rot(K, t_w_c, rot, X_w):
+def project_points_exp_rot(K, T_w_c, rot, X_w):
     if T_w_c.shape == (3, 4):
         T_w_c = np.vstack([T_w_c, [0, 0, 0, 1]])
 
@@ -179,9 +176,9 @@ def project_points_exp_rot(K, t_w_c, rot, X_w):
 
     # Rt = np.linalg.inv(T_w_c)
     
-    R_w_c = np.expm(crossMatrix(rot))
+    R_w_c = sc.linalg.expm(crossMatrix(rot))
 
-    T = ensamble_T(R_w_c, t_w_c)    
+    T = ensamble_T(R_w_c, T_w_c)    
     x_proj = project_points(K, T, X_w)
     
     return x_proj
@@ -275,6 +272,24 @@ def decompose_essential_matrix(E):
 
 
 
+def residual_bundle_adjustmentNormalRot(params, K, x1Data, x2Data, x3Data):
+
+    T_wc1 = params[:12].reshape(3, 4)
+    T_wc2 = params[12:24].reshape(3, 4)
+    T_wc3 = params[24:36].reshape(3, 4)
+    X_w = params[36:].reshape(-1, 3).T
+    
+    x1_proj = project_points(K, T_wc1, X_w)
+    x2_proj = project_points(K, T_wc2, X_w)
+    x3_proj = project_points(K, T_wc3, X_w)
+    
+    residuals = np.hstack((
+        (x1_proj[:2, :] - x1Data).flatten(),
+        (x2_proj[:2, :] - x2Data).flatten(),
+        (x3_proj[:2, :] - x3Data).flatten()
+    ))
+
+    return residuals
 
 def residual_bundle_adjustment(params, K, x1Data, x2Data, x3Data):
     t1 = params[:3]
@@ -284,7 +299,7 @@ def residual_bundle_adjustment(params, K, x1Data, x2Data, x3Data):
     t3 = params[12:15]
     r3 = params[15:18]
 
-    X_w = params[36:].reshape(-1, 3).T
+    X_w = params[18:].reshape(-1, 3).T
     
     x1_proj = project_points_exp_rot(K, t1, r1, X_w)
     x2_proj = project_points_exp_rot(K, t2, r2, X_w)
@@ -297,6 +312,32 @@ def residual_bundle_adjustment(params, K, x1Data, x2Data, x3Data):
     ))
 
     return residuals
+
+
+def run_bundle_adjustmentFull(T_wc1, T_wc2, T_wc3, K, X_w, x1Data, x2Data, x3Data):
+    
+    if X_w.shape[0] == 4:
+        X_w = (X_w[:3, :] / X_w[3, :])
+    
+    T_wc1_flat = T_wc1[:3, :].flatten()
+    T_wc2_flat = T_wc2[:3, :].flatten()
+    T_wc3_flat = T_wc3[:3, :].flatten()
+    X_w_flat = X_w.T.flatten()
+
+    initial_params = np.hstack((T_wc1_flat, T_wc2_flat, T_wc3_flat, X_w_flat))
+    
+    result = scOptim.least_squares(residual_bundle_adjustmentNormalRot, initial_params,
+                                   args=(K, x1Data, x2Data, x3Data), method='lm')
+    
+    optimized_params = result.x
+    T_wc1_opt = optimized_params[:12].reshape(3, 4)
+    T_wc2_opt = optimized_params[12:24].reshape(3, 4)
+    T_wc3_opt = optimized_params[24:36].reshape(3, 4)
+    X_w_opt = optimized_params[36:].reshape(-1, 3).T
+    
+    return T_wc1_opt, T_wc2_opt, T_wc3_opt, X_w_opt
+
+
 
 def run_bundle_adjustment(T_wc1, T_wc2, T_wc3, K, X_w, x1Data, x2Data, x3Data):
     if X_w.shape[0] == 4:
@@ -329,7 +370,7 @@ def run_bundle_adjustment(T_wc1, T_wc2, T_wc3, K, X_w, x1Data, x2Data, x3Data):
     th2_opt = optimized_params[9:12]
     t3_opt = optimized_params[12:15]
     th3_opt = optimized_params[15:18]
-    X_w_opt = optimized_params[36:].reshape(-1, 3).T
+    X_w_opt = optimized_params[18:].reshape(-1, 3).T
 
     R1_opt = expm(crossMatrix(th1_opt))
     R2_opt = expm(crossMatrix(th2_opt))
@@ -478,7 +519,8 @@ def resBundleProjectionThreeViews(Op, x1Data, x2Data, x3Data, K_c, nPoints):
 
 
 
-def visualize_matches_with_threshold(path_image_1, path_image_2, maxDist, distRatio):
+
+def visualize_matches(path_image_1, path_image_2, maxDist, distRatio, draw=True):
     image1 = cv2.imread(path_image_1)
     image2 = cv2.imread(path_image_2)
 
@@ -490,15 +532,26 @@ def visualize_matches_with_threshold(path_image_1, path_image_2, maxDist, distRa
     matchesList = matchWith2NDRR(descriptors1, descriptors2, distRatio=distRatio, maxDist=maxDist)
     dMatchesList = indexMatrixToMatchesList(matchesList)
     dMatchesList = sorted(dMatchesList, key=lambda x: x.distance)
-    
+
     # Dibujar los primeros 100 emparejamientos
     img_matched = cv2.drawMatches(
         image1, keypoints1, image2, keypoints2, dMatchesList[:100], None, 
         flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS and cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS
     )
+
+    if draw:
+        plt.imshow(img_matched, cmap='gray', vmin=0, vmax=255)
+        plt.title(f"Emparejamientos con distRatio = {distRatio}")
+        plt.subplots_adjust(
+            top=0.985,     # Border for top
+            bottom=0.015,  # Border for bottom
+            left=0.028,    # Border for left
+            right=0.992,   # Border for right
+        )
+        plt.draw()
+        plt.waitforbuttonpress()
     
-    
-    return dMatchesList, keypoints1, keypoints2
+    return dMatchesList, keypoints1, keypoints2  # Retornar las coincidencias para análisis posterior
 
 
 
@@ -526,5 +579,110 @@ def matchWith2NDRR(desc1, desc2, distRatio, maxDist=100):
         if d1 < d2 * distRatio and d1 < maxDist:
             matches.append([kDesc1, indexSort[0], d1])
     
-    return matches
+    return np.array(matches)
 
+
+def ransac_fundamental_matrix(matches, num_iterations, threshold):
+    best_F = None
+    best_inliers_count = 0
+    best_inliers = []
+
+    for _ in range(num_iterations):
+        # Seleccionar 8 puntos aleatorios
+        sample_idx = np.random.choice(matches.shape[0], 8, replace=False)
+        src_points = matches[sample_idx, :3].T
+        dst_points = matches[sample_idx, 3:6].T
+
+        # Calcular la matriz fundamental con 8 puntos seleccionados
+        F = estimate_fundamental_8point(src_points, dst_points)
+
+        # Calcular el error de transferencia para todos los emparejamientos
+        inliers = []
+        for i in range(matches.shape[0]):
+            x1 = np.append(matches[i, :2], 1)  # Punto en la primera imagen (homogéneo)
+            x2 = np.append(matches[i, 2:4], 1) # Punto en la segunda imagen (homogéneo)
+
+            l2 = F @ x1  # Línea epipolar en la segunda imagen
+            l2 /= np.sqrt(l2[0]**2 + l2[1]**2)  # Normalizar la línea
+            error = line_point_distance(l2, x2[:2])
+
+            if error < threshold:
+                inliers.append(i)
+
+        # Actualizar la mejor matriz fundamental
+        if len(inliers) > best_inliers_count:
+            best_F = F
+            best_inliers_count = len(inliers)
+            best_inliers = inliers
+
+    return best_F, best_inliers
+
+
+def line_point_distance(line, point):
+    """
+    Calcula la distancia entre una línea y un punto.
+    
+    Args:
+        line: Coeficientes de la línea, tamaño (3,).
+        point: Coordenadas del punto, tamaño (2,).
+        
+    Returns:
+        float: Distancia entre la línea y el punto.
+    """
+
+    a, b, c = line
+    x, y = point
+    return abs(a*x + b*y + c) / np.sqrt(a**2 + b**2)
+
+
+
+def do_matches(path_image_1='images/image1.png', path_image_2='images/image2.png', option=0, draw=False):
+    if option == 0:
+        path = './results/image1_image2_matches.npz'
+        npz = np.load(path)
+
+        keypoints1 = npz['keypoints0'] 
+        keypoints2 = npz['keypoints1']  
+        matches = npz['matches']
+
+        valid_matches_idx = matches > -1
+        keypoints1_matched = keypoints1[valid_matches_idx]
+        keypoints2_matched = keypoints2[matches[valid_matches_idx]]
+
+        keypoints_cv1 = [cv2.KeyPoint(x=pt[0], y=pt[1], size=1) for pt in keypoints1_matched]
+        keypoints_cv2 = [cv2.KeyPoint(x=pt[0], y=pt[1], size=1) for pt in keypoints2_matched]
+
+        # Crear objetos DMatch con índices secuenciales
+        dMatchesList = [cv2.DMatch(_queryIdx=i, _trainIdx=i, _distance=0) for i in range(len(keypoints_cv1))]
+
+
+        # Convierte los emparejamientos a coordenadas (x, y)
+        srcPts = keypoints1_matched  # Ya es un array con coordenadas (x, y)
+        dstPts = keypoints2_matched
+        x1 = np.vstack((srcPts.T, np.ones((1, srcPts.shape[0]))))
+        x2 = np.vstack((dstPts.T, np.ones((1, dstPts.shape[0]))))
+
+        matched_points = np.hstack((x1, x2))
+        matched_points = np.hstack((srcPts, dstPts))
+
+    elif option == 1:
+        distRatio = 0.75
+        maxDist = 500
+        dMatchesList, keypoints1, keypoints2 = visualize_matches(path_image_1, path_image_2, maxDist, distRatio, draw=draw)
+
+        print("Total de keypoints en la primera imagen:", len(keypoints1))
+        print("Total de keypoints en la segunda imagen:", len(keypoints2))
+
+        # Convierte los emparejamientos a coordenadas (x, y)
+        srcPts = np.float32([keypoints1[m.queryIdx].pt for m in dMatchesList]).T
+        dstPts = np.float32([keypoints2[m.trainIdx].pt for m in dMatchesList]).T
+        x1 = np.vstack((srcPts, np.ones((1, srcPts.shape[1])))).T
+        x2 = np.vstack((dstPts, np.ones((1, dstPts.shape[1])))).T
+
+        matched_points = np.hstack((x1, x2))
+        #matched_points = np.hstack((srcPts, dstPts))
+
+    else:
+        matched_points = None
+
+    return matched_points, srcPts, dstPts
