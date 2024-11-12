052259c30c080bcd15375471448cd8d884af991c
hsunekichi <hsunekichi@gmail.com>
2024-11-13 00:56:26 +0100

    Partial implementation of 3 camera sfm, not working

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,9 +1,11 @@
 import numpy as np
+import cv2
 import lib.data_process as data_process
 import lib.geometric_math as math
 import lib.plot as plot
 import lib.sfm as sfm
 import lib.matching as matching
+import lib.projective as projective
 
 work_dir = "/home/hsunekichi/Escritorio/Computer_Vision_Practicas/p4/"
 
@@ -40,25 +42,51 @@ image1 = data_process.read_image(im1_pth)
 image2 = data_process.read_image(im2_pth)
 image3 = data_process.read_image(im3_pth)
 
-kpts1, kpts2, x1, x2 = matching.match_superglue(image1, image2, model, device)
-
+x1_2, x2_1, mask1, spData1, spData2 = matching.match_superglue(image1, image2, model, device)
+x2_3, x3_2, mask3, spData2, spData3 = matching.match_superglue(image2, image3, model, device, spData2)
 #plot.visualize_matches(image1, image2, x1_rand, x2_rand, 'Matches')
 
-T_wc1, T_wc2, X_w = sfm.bundlePoseEstimation(x1, x2, K_c)
+T_wc1, T_wc2, X_w = sfm.bundlePoseEstimation(x1_2, x2_1, K_c)
 
 
-P1 = math.projectionMatrix(K_c, T_wc1)
-P2 = math.projectionMatrix(K_c, T_wc2)
+if X_w.shape[1] < 4:
+    raise ValueError("Se requieren al menos 4 puntos para solvePnP con SOLVEPNP_EPNP")
 
-X_w = sfm.triangulate_points(P1, P2, x1, x2)
+objectPoints = X_w.T[mask3].astype(np.float64)
+#imagePoints = np.ascontiguousarray(x3[0:2,:].T).reshape((x3.shape[1], 1, 2))
+imagePoints = x3_2.T
+
+distCoeffs = np.zeros((4, 1), dtype=np.float64)
+retval, rvec, tvec = cv2.solvePnP(
+    objectPoints, 
+    imagePoints, 
+    K_c, 
+    distCoeffs, 
+    flags=cv2.SOLVEPNP_EPNP
+)
+
+#R = utils.rotvec_to_rotmat(rvec)
+R, _ = cv2.Rodrigues(rvec)
+print("Matriz de rotación R:", R)
+print("Vector de traslación t:", tvec)
+
+T_wc3 = np.eye(4)
+T_wc3[:3, :3] = R 
+T_wc3[:3, 3] = tvec.flatten()
+
+T_cw3 = T_wc3.copy()
+T_wc3 = np.linalg.inv(T_cw3)
+
 
 
 # Step 6: Visualize Optimized Projection
-x1_proj = math.project_points(K_c, T_wc1, X_w)
-x2_proj = math.project_points(K_c, T_wc2, X_w)
+x1_proj = projective.project_points(K_c, T_wc1, X_w)
+x2_proj = projective.project_points(K_c, T_wc2, X_w)
+x3_proj = projective.project_points(K_c, T_wc3, X_w)
 
-plot.visualize_projection(image1, x1, x1_proj, 'Image 1', False)
-plot.visualize_projection(image2, x2, x2_proj, 'Image 2', False)
+plot.visualize_projection(image1, x1_2, x1_proj, 'Image 1', False)
+plot.visualize_projection(image2, x2_1, x2_proj, 'Image 2', False)
+plot.visualize_projection(image3, x3_2, x3_proj, 'Image 3', False)
 
-plot.plot_3D_scene([T_wc1, T_wc2], X_w)
+#plot.plot_3D_scene([T_wc1, T_wc2], X_w)
 
